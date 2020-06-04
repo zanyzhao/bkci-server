2eeb17327837233ad3e9ee539068fdb96b5c47e1
zanyzhao <zanyzhao@gmail.com>
2020-06-04 15:49:24 +0800

    Update launch_services.sh

--- scripts/launch_services.sh
+++ scripts/launch_services.sh
@@ -1,9 +1,11 @@
 cd /data/docker/bkci/ci/ || exit 1
 
+sed -i 's/2g/256m/g' */boot-*.sh
+
 for i in */boot-*.sh;do
   case $i in
     *notify*) continue;;
     *openapi*) continue;;
-    *) sed -i 's/2g/256m/g' $i && bash $i $1
+    *) bash $i $1
   esac
 done
